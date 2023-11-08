999344c80c29b31d17f9a42c89bdaacedde549cf
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2023-11-08 00:24:56 +0000

    create users.sql

---
+++ backend/testData/users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users(
+    id serial PRIMARY KEY,
+    user_full_name varchar(200) NOT null);
+
+INSERT INTO users(user_full_name)
+    VALUES 
+    ('Afsha Rose'),
+    ('Shadi Sunflower'),
+    ('Junita Bluebells'),
+    ('Alexey Orchid'),
+   	('Alisha Jasmine'),
+   	('Barath Lupine');
