e34cc13a09fe9a6af1f2b2f409faf0b1add820dc
Afsha10 <unityofbeing1@gmail.com>
2023-11-11 16:55:15 +0000

    Chore: Set up Tailwind for styling our UI
    * Install and test Tailwind on "Hello World"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,8 @@
-
-
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!!! </h1>
+        <h1 className=" text-3xl font-bold ">Hello World!!! </h1>
       </header>
     </div>
   );
