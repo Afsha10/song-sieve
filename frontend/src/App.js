3916fc0ed3b17c4c5eb7921d3fec5829212a328f
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-15 19:51:00 +0000

    formatted app.js
    deleted extra line in app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <RouterProvider
           router={AppRouter}
           fallbackElement={<p>Loading...</p>}
