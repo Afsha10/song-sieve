51dba21f46d7b4897f78999f5d2ebd61219421e0
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-17 11:52:23 +0000

    updated homepage
    *changed everything to tailwind
    *added styling in homepage

--- frontend/src/components/screens/LoginScreen.jsx
+++ frontend/src/components/screens/LoginScreen.jsx
@@ -3,6 +3,7 @@ import { generateRandomString } from "../../utils/encodingUtils";
 import { sha256 } from "../../utils/encodingUtils";
 import { base64encode } from "../../utils/encodingUtils";
 import { redirectToSpotify } from "../../utils/spotifyApiUtils";
+import bg from "../images/hero.jpg";
 // import "dotenv/config";
 
 function LoginScreen() {
@@ -24,18 +25,26 @@ function LoginScreen() {
   }, []);
 
   return (
-    <div className="homepage">
-      <div class="block rounded-lg p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-        <p class="mb-4 text-base to-blacktext-neutral-600 dark:text-neutral-200">
+    <div className="grow bg-red-200">
+      <div className="m-8 text-justify font-sm md:font-semibold md:text-2xl lg:text-4xl text-gray-600">
+        <p className="m-4">
           Hello sunshine !! This app will help you to bring a playlist and
           create your own playlist with the tracks you love. Sign up to save
           your playlist, then upload your customised playlist to your Spotify
           account.
         </p>
-        <div class="h-screen flex items-center justify-center">
+        <div>
+          <img
+            src={bg}
+            alt="Spotify logo"
+            className="flex items-center h-64 sm:mx-7 sm:h-20 md:mx-auto md:my-8 md:h-80 lg:h-96 lg:h-24"
+          />
+        </div>
+
+        <div className="flex flex-col">
           <button
+            className=" bg-white text-blue-500 font-extrabold py-1 px-4 rounded-full mt-3 md:mt-20 border"
             type="button"
-            class="inline-block rounded border-3 border-primary px-56 pb-[10px] pt-2 text-m font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-800 focus:border-primary-600 focus:text-primary-800 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-900 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
             data-te-ripple-init
             onClick={() =>
               redirectToSpotify(
@@ -54,3 +63,5 @@ function LoginScreen() {
 }
 
 export default LoginScreen;
+
+
