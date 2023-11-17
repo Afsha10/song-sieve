19f5faeb9163411b3f4f72951157684352f2bc16
Afsha10 <unityofbeing1@gmail.com>
2023-11-17 17:06:29 +0000

    Create a footer component  and fix a tiny bug
    * Create a footer and make it work
    * Remove header tag wrapping the RouterProvide because the RouterProvider gives access to the main content (RouterProvider does not function as a navigation)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./AppRouter";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className=" flex flex-col bg-yellow-100 min-h-screen">
-      <header className="App-header">
-        <RouterProvider
-          router={AppRouter}
-          fallbackElement={<p>Loading...</p>}
-        />
-      </header>
+      <header className="App-header"></header>
+      {/* RouterProvider below is the main content */}
+      <RouterProvider
+        className="grow"
+        router={AppRouter}
+        fallbackElement={<p>Loading...</p>}
+      />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="bg-red-200 p-4 text-center text-xs text-black w-screen md:text-base lg:text-lg mt-auto">
+        <p>&copy; 2023 Spotify Playlist Sieve</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
