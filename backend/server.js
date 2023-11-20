76ec9d6036e35238b8adfe9547fe40eb871a35b0
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2023-11-20 12:02:08 +0000

    final changes

--- backend/server.js
+++ backend/server.js
@@ -12,53 +12,26 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// credentials for database
-// const db = new Pool({
-//   user: process.env.POSTGRES_USERNAME,
-//   host: process.env.POSTGRES_HOST,
-//   database: process.env.POSTGRES_DATABASE,
-//   password: process.env.POSTGRES_PASSWORD,
-//   port: process.env.POSTGRES_PORT,
-//   ssl: true,
-// });
+//credentials for database
+const db = new Pool({
+  user: process.env.POSTGRES_USERNAME,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  ssl: true,
+});
 
-// Connecting to database
-// db.connect(function (err) {
-//   if (err) throw err;
-//   console.log("Connected to database !");
-// });
+//Connecting to database
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to database !");
+});
 
 
 app.get("/", (req, res) => {
   res.status(200).json("Hello World!");
 });
-// refresh an access token
-app.get('/refresh_token', function(req, res) {
 
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-    },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token,
-          refresh_token = body.refresh_token;
-      res.send({
-        'access_token': access_token,
-        'refresh_token': refresh_token
-      });
-    }
-  });
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
