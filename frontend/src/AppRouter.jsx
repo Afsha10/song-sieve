89ff2dd991a8a95f094087d5176a17652a950279
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-22 00:27:58 +0000

    Update AppRouter.jsx

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -3,12 +3,12 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-
 import "./index.css";
 import LoginScreen from "./components/screens/LoginScreen";
 import MainScreen from "./components/screens/MainScreen";
 
-let AppRouter = createBrowserRouter(
+// Configure nested routes with JSX
+const AppRouter = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<LoginScreen />} />
