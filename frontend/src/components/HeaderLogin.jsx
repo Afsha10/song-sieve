c2b9030f1666fcc368b24e934b8beee4a50052b8
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-23 16:57:42 +0000

    Update HeaderLogin.jsx

--- frontend/src/components/HeaderLogin.jsx
+++ frontend/src/components/HeaderLogin.jsx
@@ -7,7 +7,7 @@ const HeaderLogin = () => {
                 src="https://i.pinimg.com/736x/17/d2/5d/17d25d4df677597d4ed0db654a4179a6.jpg"
                     alt="Spotify Logo" />  
                 <div>
-                    <p>Spotifay Playlist Seive</p>
+                    <p>Spotify Playlist Seive</p>
                 </div>
                 <div>
                     <p>Hi</p>
