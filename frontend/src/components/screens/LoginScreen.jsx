bc1b91238018494fbd67c278d067047432e43ecd
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2023-11-23 10:23:28 +0000

    final change

--- frontend/src/components/screens/LoginScreen.jsx
+++ frontend/src/components/screens/LoginScreen.jsx
@@ -25,9 +25,13 @@ function LoginScreen() {
   }, []);
 
   return (
+    <div>
+      <HeaderLogin/>
+    <div style={{display:"flex",justifyContent:"center"}}>
+      
     <div className="mainLoginDiv">
             <div className="firstLoginDiv">
-                  <HeaderLogin/>
+                 
                   <p id="textLogin">
                     Hello sunshine !! This app will help you to bring a playlist and create
                     your own playlist with the tracks you love. Sign up to save your
@@ -50,41 +54,12 @@ function LoginScreen() {
                   </div>
             </div>
             <div className="secondLoginDiv" >
-                    <img src="https://static6.depositphotos.com/1036464/601/i/450/depositphotos_6011415-stock-photo-beautiful-girl-is-listen-to.jpg"/>
+                    <img src="https://cdn2.psychologytoday.com/assets/styles/manual_crop_1_91_1_1528x800/public/blogs/30322/2009/07/31495-12668.jpg?itok=31b-A7bj"/>
             </div>
         
     </div>
-    // <div className="flex flex-col grow bg-yellow-100 justify-between m-8 text-justify font-sm md:font-semibold md:text-2xl lg:text-3xl text-gray-600">
-    //    <HeaderLogin/>
-    //   <p className="lg:mx-auto lg:w-3/5 mb-2 lg:mb-5">
-    //     Hello sunshine !! This app will help you to bring a playlist and create
-    //     your own playlist with the tracks you love. Sign up to save your
-    //     playlist, then upload your customised playlist to your Spotify account.
-    //   </p>
-    //   <div>
-    //     <img
-    //       src="https://cdn.pixabay.com/photo/2016/04/05/06/28/clef-1308793_640.jpg"
-    //       alt="Spotify logo"
-    //       className="flex items-center h-64 sm:mx-7 sm:h-20 md:mx-auto md:h-full"
-    //     />
-    //   </div>
-
-      // <div className="flex flex-col md:items-center">
-      //   <button
-      //     className="signInButton "
-          
-      //     onClick={() =>
-      //       redirectToSpotify(
-      //         localStorage.getItem("codeVerifier"),
-      //         localStorage.getItem("codeChallenge"),
-      //         "user-read-private playlist-read-private user-read-email"
-      //       )
-      //     }
-      //   >
-      //     Sign in
-      //   </button>
-      // </div>
-    // </div>
+    </div> 
+    </div>
   );
 }
 
