4e87fcceea47fb53f90110515bbbd71485a36123
Afsha10 <unityofbeing1@gmail.com>
2023-11-23 23:08:58 +0000

    Changed the modal background colour to white as in Hsuan's design

--- frontend/src/components/dialogs/PlaylistTracksFilterModal.jsx
+++ frontend/src/components/dialogs/PlaylistTracksFilterModal.jsx
@@ -8,7 +8,7 @@ function PlaylistTracksFilterModal({ isOpen, handleModalOpen }) {
       open={isOpen}
       onClose={() => handleModalOpen(false)}
     >
-      <Dialog.Panel className="absolute top-1/2  left-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 w-4/5 bg-orange-400 z-10 p-6">
+      <Dialog.Panel className="absolute top-1/2  left-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 w-4/5 bg-white z-10 p-6">
         <Dialog.Title className="text-2xl m-3">Filter by tracks</Dialog.Title>
         <div className="m-3 text-lg">
           <Dialog.Description className="text-lg">Genre</Dialog.Description>
@@ -30,13 +30,13 @@ function PlaylistTracksFilterModal({ isOpen, handleModalOpen }) {
 
         <div className="flex flex-row justify-center m-auto my-4">
           <button
-            className="ml-3 inline-flex justify-center rounded-md border-2 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto"
+            className="ml-3 inline-flex justify-center rounded-md border-2 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto"
             onClick={() => handleModalOpen(false)}
           >
             Clean
           </button>
           <button
-            className="ml-3 inline-flex justify-center rounded-md bg-light-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto"
+            className="ml-3 inline-flex justify-center rounded-md bg-light-blue-700 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-blue-700 sm:ml-3 sm:w-auto"
             onClick={() => handleModalOpen(false)}
           >
             Apply to playlist
