6495b05a97f3f83ccceea5bdfdd86be204c055c5
Afsha10 <unityofbeing1@gmail.com>
2023-11-23 03:00:08 +0000

    Renamed the page to something more suitable

--- frontend/src/components/screens/PlaylistTracksScreen.jsx
+++ frontend/src/components/screens/PlaylistTracksScreen.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 // import PlaylistTracksFilterModal from "../dialogs/PlaylistTracksFilterModal";
 
-function PlaylistTracksFilterScreen() {
+function PlaylistTracksScreen() {
     
   return (
     <div className=" bg-orange-200 grow">
-      <h2>PlaylistTracksFilterScreen</h2>
+      <h2>PlaylistTracksScreen</h2>
       {/* <PlaylistTracksFilterModal /> */}
     </div>
   );
 }
 
-export default PlaylistTracksFilterScreen;
+export default PlaylistTracksScreen;
