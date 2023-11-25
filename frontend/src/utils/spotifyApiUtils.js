a75ab73109b6ce0b2cf07e83fd2bd5b6fa06990e
Afsha10 <unityofbeing1@gmail.com>
2023-11-25 16:20:25 +0000

    Fix the bug to redirect the page to current page
    * We do this so that we are not redirected to our localhost when we are in https://song-sieve-frontend.onrender.com/

--- frontend/src/utils/spotifyApiUtils.js
+++ frontend/src/utils/spotifyApiUtils.js
@@ -1,6 +1,5 @@
-export function redirectToSpotify(codeVerifier, codeChallenge, scope) {
+export function redirectToSpotify(codeVerifier, codeChallenge, scope, redirectUri) {
   const clientId = "719d232ba04d433d98b3605bf4b316e1";
-  const redirectUri = "http://localhost:3000/app";
   const authUrl = new URL("https://accounts.spotify.com/authorize");
 
   window.localStorage.setItem("code_verifier", codeVerifier);
