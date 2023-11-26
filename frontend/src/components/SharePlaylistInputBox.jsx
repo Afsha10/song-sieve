a9796d70c2804e5885d05cafd89025093982714c
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-26 11:30:10 +0000

    testing for render

--- frontend/src/components/SharePlaylistInputBox.jsx
+++ frontend/src/components/SharePlaylistInputBox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const SharePlaylistInputBox = () => {
   let navigate = useNavigate();
@@ -22,7 +22,7 @@ const SharePlaylistInputBox = () => {
     // const myAccessToken = localStorage.getItem("access_token");
     // const myTokenType = localStorage.getItem("token_type");
 
-    // 
+    //
   };
 
   return (
@@ -30,24 +30,21 @@ const SharePlaylistInputBox = () => {
       <div className="grid gap-2 text-sm md:text-xl lg:text-3xl">
         <form className="flex flex-col m-auto md:mx-24 lg:mx-96 mx-">
           <p className="m-2">Add your Spotify playlist URL</p>
-  
-            <input
-              type="text"
-              placeholder="Playlist URL"
-              className="h-8 w-full rounded bg-gray-200 p-2 pl-4 pr-4 lg:h-12 lg:m-2"
-              onChange={(e) => setInputUrl(e.target.value)}
-            />
-              <button
-                className="relative cursor-default rounded-md bg-blue-800 px-10 py-1 text-center self-center text-white shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 my-3"
-                type="submit"
-                onClick={(e) => handleSubmit(e)}
-              >
-                Import shared playlist
-              </button>
-           
-          
+
+          <input
+            type="text"
+            placeholder="Playlist URL"
+            className="h-8 w-full rounded bg-gray-200 p-2 pl-4 pr-4 lg:h-12 lg:m-2"
+            onChange={(e) => setInputUrl(e.target.value)}
+          />
+          <button
+            className="relative cursor-default rounded-md bg-blue-800 px-10 py-1 text-center self-center text-white shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 my-3"
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Import shared playlist
+          </button>
         </form>
-       
       </div>
 
       {error && <p>{error}</p>}
