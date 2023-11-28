f5ddecbe58f8ad517d492da68b3ebb1ecc59ed76
shadi fakhri <113238379+Shadi38@users.noreply.github.com>
2023-11-28 15:08:43 +0000

    final changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import { RouterProvider } from "react-router-dom";
 import AppRouter from "./AppRouter";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer"
 
 
 
 function App() {
   return (
-    <div className=" flex flex-col bg-black min-h-screen">
-      
-      {/* RouterProvider below is the main content */}
-      <RouterProvider
-        router={AppRouter}
-        fallbackElement={<p>Loading...</p>}
-      />
-      <div className="footer">
+    <div>
+      <div className=" flex flex-col bg-black min-h-screen">
+        {/* RouterProvider below is the main content */}
+        <RouterProvider
+          router={AppRouter}
+          fallbackElement={<p>Loading...</p>}
+        />
+      </div>
+      {/* <div className="footer"> */}
+      <div>
         <Footer/>
       </div>
-      
     </div>
   );
 }
