277680bf2a9792b0b65001d97aa16dbb2f6ec89b
Afsha10 <unityofbeing1@gmail.com>
2023-11-29 18:34:41 +0000

    Replace typo error for closing the iframe with a cross symbol
    * Replace typo error for closing the iframe with a cross symbol to indicate close iframe

--- frontend/src/components/SharedPlaylistDisplay.jsx
+++ frontend/src/components/SharedPlaylistDisplay.jsx
@@ -68,41 +68,49 @@ const SharedPlaylistDisplay = ({ playlistData }) => {
         </div>
       </div>
       <div className="grid grid-cols-1 gap-10 my-14 md:grid-cols-2 lg:grid-cols-4 md:gap-10 text-xl md:text-3xl md:mx-6">
-      {playTracks && (
+        {playTracks && (
           <div id="embed-iframe">
-            <iframe src={`https://open.spotify.com/embed/track/${urlTrack}`} width="280" height="100" ></iframe>
-          <button style={{position:"relative"}} onClick={()=>setPlayTracks(false)}>cancle</button>
+            <iframe
+              src={`https://open.spotify.com/embed/track/${urlTrack}`}
+              width="280"
+              height="100"
+            ></iframe>
+            <button
+              style={{ position: "relative" }}
+              onClick={() => setPlayTracks(false)}
+            >
+              ❌
+            </button>
           </div>
         )}
-      {playlistData.tracks.items.map((track, trackIndex) => (
-        <div
-          key={trackIndex}
-          className="grid grid-cols-2 
+        {playlistData.tracks.items.map((track, trackIndex) => (
+          <div
+            key={trackIndex}
+            className="grid grid-cols-2 
         md:gap-5
         text-xl
         md:text-3xl"
-        >
-          {track.track.album.images.length > 0 && (
-            
-            <img
-              key={trackIndex}
-              src={track.track.album.images[0].url}
-              alt={`Album Cover for ${track.track.name}`}
-              style={{ cursor: "pointer" }}
-              onClick={() => handlePlay(track.track.uri, track.track.id)}
-            />
-          )}
+          >
+            {track.track.album.images.length > 0 && (
+              <img
+                key={trackIndex}
+                src={track.track.album.images[0].url}
+                alt={`Album Cover for ${track.track.name}`}
+                style={{ cursor: "pointer" }}
+                onClick={() => handlePlay(track.track.uri, track.track.id)}
+              />
+            )}
 
-          <div className="grid grid-rows">
+            <div className="grid grid-rows">
               <p className="font-bold text-blue-400">{track.track.name}</p>
               <p className=" text-gray-400">
                 {formatDuration(track.track.duration_ms)}
               </p>
-          </div>        
-        </div>
-      ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)};
+  );};
 
 export default SharedPlaylistDisplay;
