5ee5d7ca050ebf7b6b3b85408cc5814df0b51270
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-11-29 11:11:49 +0000

    added clickhandler

--- frontend/src/components/ContentCheckBox.jsx
+++ frontend/src/components/ContentCheckBox.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ContentCheckbox = ({ label, checked, onChange }) => {
+const ContentCheckbox = ({ playlistData }) => {
+  const [filteredTracks, setFilteredTracks] = useState([]);
+  const [yesChecked, setYesChecked] = useState(false);
+  const [noChecked, setNoChecked] = useState(false);
+
+  function yesClickHandler() {
+    //console.log(playlistData.tracks.items)
+    //console.log(playlistData.tracks.items[0].track.explicit)
+    const selectedTracks = playlistData.tracks.items.filter(
+      (track) => track.track.explicit === true
+    );
+    //console.log(selectedTracks)
+    setFilteredTracks(selectedTracks);
+    setNoChecked(false);
+    setYesChecked(true);
+  }
+  function noClickHandler() {
+    console.log(playlistData.tracks.items);
+    console.log(playlistData.tracks.items[0].track.explicit);
+    const selectedTracks = playlistData.tracks.items.filter(
+      (track) => track.track.explicit === false
+    );
+    console.log(selectedTracks);
+    setFilteredTracks(selectedTracks);
+    setYesChecked(false);
+    setNoChecked(true);
+  }
+
   return (
     <div>
       <label>
         Yes
-        {label}
-        <input type="checkbox" checked={checked} onChange={onChange} />
+        <input
+          type="checkbox"
+          checked={yesChecked}
+          onChange={yesClickHandler}
+        />
       </label>
       <label>
         No
-        {label}
-        <input type="checkbox" checked={checked} onChange={onChange} />
+        <input type="checkbox" checked={noChecked} onChange={noClickHandler} />
       </label>
     </div>
   );
