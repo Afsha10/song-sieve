59e271991d44febba880438cb9264ee6b6485ca5
Afsha10 <unityofbeing1@gmail.com>
2024-01-04 14:59:34 +0000

    Help the testers login to our app
    - Add the credentials of a dummy Spotify account to the login screen
    - Add an alert box with the login info

--- frontend/src/components/screens/LoginScreen.jsx
+++ frontend/src/components/screens/LoginScreen.jsx
@@ -34,6 +34,39 @@ function LoginScreen() {
               your playlist, then upload your customised playlist to your
               Spotify account.
             </p>
+
+            {/* testers login info */}
+            <div
+              class="bg-blue-950 border-t-4 border-green-500 rounded-b text-teal-900 px-4 py-3 shadow-md m-2 sm:mx-24 sm:my-3 lg:mx-22 lg:my-10 "
+              role="alert"
+            >
+              <div class="flex">
+                <div class="py-1">
+                  <svg
+                    class="fill-current h-6 w-6 text-green-500 mr-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                  </svg>
+                </div>
+                <div>
+                  <p class="text-sm">
+                    If you want to test our service and don't have a Spotify
+                    account, please use the following credentials:
+                    <ul>
+                      <li>
+                        Email: <code>cyfqueens@gmail.com</code>
+                      </li>
+                      <li>
+                        Password: <code>CodeYourDreams</code>
+                      </li>
+                    </ul>
+                  </p>
+                </div>
+              </div>
+            </div>
+
             <div className="divLoginBtn">
               <div>
                 <button
